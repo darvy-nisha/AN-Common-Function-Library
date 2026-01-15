e50bdc6af3fa0c712eaeab5fa0bc47fa6557df4a
darvy-nisha <darvy.nisha@gmail.com>
2026-01-15 19:26:24 +0530

    test

--- LoginPage.py
+++ LoginPage.py
@@ -1,26 +1,30 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class Login:
-    def __init__(self,driver):
-        self.text_invalidmsg_xpath
+    def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
     textbox_username_xpath = (By.XPATH, "//input[@placeholder='Username']")
     textbox_password_xpath = (By.XPATH, "//input[@placeholder='Password']")
     button_login_xpath = (By.XPATH, "//button[@type='submit']")
     invalid_msg_xpath = (By.XPATH, "//p[@class='oxd-text oxd-text--p oxd-alert-content-text']")
 
-    def input_username(self,username):
-        self.driver.find_element(*self.textbox_username_xpath).send_keys(username)
+    def input_username(self, username):
+        username_field = self.wait.until(EC.visibility_of_element_located(self.textbox_username_xpath))
+        username_field.clear()
+        username_field.send_keys(username)
 
-    def input_password(self,password):
-        self.driver.find_element(*self.textbox_password_xpath).send_keys(password)
+    def input_password(self, password):
+        password_field = self.wait.until(EC.visibility_of_element_located(self.textbox_password_xpath))
+        password_field.clear()
+        password_field.send_keys(password)
 
     def click_login(self):
-        self.driver.find_element(*self.button_login_xpath).click()
+        self.wait.until(EC.element_to_be_clickable(self.button_login_xpath)).click()
 
     def invalid_mssg(self):
-        return self.driver.find_element(*self.invalid_msg).text
-
-
+        return self.wait.until(EC.visibility_of_element_located(self.invalid_msg_xpath)).text
