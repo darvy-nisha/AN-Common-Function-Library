c4dc9f246d5f26f7fd9ef29b8bb4ae873c7fb12f
darvy-nisha <darvy.nisha@gmail.com>
2026-01-16 20:04:14 +0530

    development for test_reset_password test file

--- LoginPage.py
+++ LoginPage.py
@@ -2,7 +2,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-class Login:
+class LoginPage:
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(self.driver, 10)
